f2cd33d06fe375ac07d3b944a0053d5dac336f1d
Nour0206Nour0206 <nourkedri0206@gmail.com>
2024-12-30 20:57:38 +0100

    add security

--- src/main/java/com/example/tptekup/Response/JwtResponse.java
+++ src/main/java/com/example/tptekup/Response/JwtResponse.java
@@ -18,6 +18,62 @@ public class JwtResponse {
 
     private List<String> roles;
 
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getRefreshToken() {
+        return refreshToken;
+    }
+
+    public void setRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<String> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<String> roles) {
+        this.roles = roles;
+    }
+
     public JwtResponse(String accessToken, String refreshToken, Long id, String username,
                        String email, List<String> roles) {
         this.token = accessToken;
